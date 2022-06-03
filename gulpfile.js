a8110661978ba76bec7d78733f8e8105dfa21748
Jason Amey <jason.amey@gmail.com>
2022-06-03 17:52:31 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+function buildStyles() {
+  return src("styles/**/*.scss").pipe(sass()).pipe(dest("css"));
+}
+
+function watchTask() {
+  watch(["styles/**/*.scss"], buildStyles);
+}
+
+exports.default = series(buildStyles, watchTask);
+
+// const { src, dest, watch, series } = require("gulp");
+// const sass = require("gulp-sass")(require("sass"));
+
+// function buildStyles() {
+//   return src("index.scss").pipe(sass()).pipe(dest("css"));
+// }
+
+// function watchTask() {
+//   watch(["index.scss"], buildStyles);
+// }
+
+// exports.default = series(buildStyles, watchTask);
